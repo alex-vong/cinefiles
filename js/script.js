d4fddf4a23889885bb8678596f8fb5b376a6e5fd
Alex Vong <avong003@gmail.com>
2023-05-01 22:48:28 -0700

    js for displaying movies when movies button clicked and tv shows when tv shows button clicked

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ const form = document.querySelector('#search-form'),
   dropDown = document.querySelector('.search-dd'),
   navSearch = document.querySelector('.nav-search'),
   closeSearch = document.querySelector('i.close-search'),
-  gridContainer = document.querySelector('.show-grid-container');
-
-dropDown.addEventListener('click', () => {
   gridContainer = document.querySelector('.show-grid-container'),
   nowPlaying = document.getElementById('now-playing'),
   trendingList = document.querySelector('.trending-ul'),
@@ -29,76 +26,221 @@ function dropDownSearch() {
     navSearch.style.zIndex = '0';
   }, 1000);
 
-  // gridContainer.style.paddingTop = '100px';
-});
+  nowPlaying.style.marginTop = '80px';
+}
 
-closeSearch.addEventListener('click', () => {
+function closeDropDownSearch() {
   if (window.innerWidth < 1000) {
     navSearch.style.zIndex = '-1000';
-    console.log('clicked');
   }
 
-  // gridContainer.style.paddingTop = '30px';
+  nowPlaying.style.marginTop = '0';
+
   navSearch.classList.remove('slide-in');
   navSearch.classList.add('slide-out');
-  navSearch.style.animationDuration = '0.3s';
-});
+  // navSearch.style.animationDuration = '0.3s';
+}
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
-  landing.style.display = 'hidden';
-  const results = await getTvShows();
-  console.log(results);
+async function displayTrending(type) {
+  let { results } = await fetchAPIData(`/trending/${type}/week`);
 
-  let li = '',
-    premiered = '',
-    img = '';
-  for (let show of results) {
-    const name = show.show.name,
-      type = show.show.type;
+  // const shuffle = shuffleArray(results);
 
-    if (show.show.premiered) {
-      premiered = show.show.premiered.substring(0, 4);
-    } else {
-      premiered = `N/A`;
-    }
+  trendingList.innerHTML = '';
 
-    if (show.show.image) {
-      img = show.show.image.medium;
-    } else {
-      img = `images/unavailable.svg`;
-    }
+  results.splice(-2);
+  results.forEach((movie) => {
+    const individualMovieCard = generateList(movie);
 
-    li += `<li>
-			       <picture>
-			       	  <img src="${img}" alt="show poster for ${name}" loading="lazy">
-			       </picture>
+    trendingList.appendChild(individualMovieCard);
+  });
 
-			       <h2>${name}</h2>
-			       <div class="preview-info">
-			       		<span>${premiered}</span>
-			       		<span>${type}</span>
+  if (type === 'movie') {
+    displayPopular(type);
+  }
+  if (type === 'tv') {
+    displayLatestReleases(type);
+  }
+}
 
-			       </div>
-			   </li>`;
+async function displayPopular(type) {
+  const { results } = await fetchAPIData(`${type}/popular`);
+  // const shuffle = shuffleArray(results);
 
-    ul.innerHTML = li;
+  results.splice(-2);
+
+  popularList.innerHTML = '';
+
+  document.querySelector('#popular-movies h2').textContent = 'Popular';
+
+  results.forEach((movie) => {
+    const individualMovieCard = generateList(movie);
+
+    popularList.appendChild(individualMovieCard);
+  });
+
+  displayUpcoming(type);
+}
+
+async function displayUpcoming(type) {
+  let { results } = await fetchAPIData(`${type}/upcoming`);
+  // const shuffle = shuffleArray(results);
+
+  results.splice(-2);
+  upcomingList.innerHTML = '';
+  document.querySelector('#upcoming-movies h2').textContent = 'Coming Soon';
+
+  results.forEach((movie) => {
+    const individualMovieCard = generateList(movie);
+
+    upcomingList.appendChild(individualMovieCard);
+  });
+}
+
+async function displayLatestReleases(type) {
+  const { results } = await fetchPopData(`discover/${type}`);
+
+  // const shuffle = shuffleArray(results);
+
+  results.splice(-2);
+  popularList.innerHTML = '';
+  document.querySelector('#popular-movies h2').textContent = 'Latest Releases';
+
+  results.forEach((movie) => {
+    const individualMovieCard = generateList(movie);
+
+    popularList.appendChild(individualMovieCard);
+  });
+
+  displayTopRated(type);
+}
+
+async function displayTopRated(type) {
+  let { results } = await fetchAPIData(`${type}/top_rated`);
+  // const shuffle = shuffleArray(results);
+
+  results.splice(-2);
+  upcomingList.innerHTML = '';
+
+  document.querySelector('#upcoming-movies h2').textContent = 'Top Rated';
+
+  results.forEach((movie) => {
+    const individualMovieCard = generateList(movie);
+
+    upcomingList.appendChild(individualMovieCard);
+  });
+}
+
+async function displayTrendingTV() {
+  displayTrending('tv');
+}
+
+function generateList(movie) {
+  const li = document.createElement('li');
+  li.classList.add('card');
+  let dateString;
+  let year;
+  let name;
+
+  if (movie.media_type === 'movie' || movie.title) {
+    name = movie.title;
+    dateString = movie.release_date;
+    year = dateString.slice(0, 4);
   }
 
-  input.value = '';
-});
+  if (movie.media_type === 'tv' || movie.name) {
+    name = movie.name;
+    dateString = movie.first_air_date;
+    year = dateString.slice(0, 4);
+  }
 
-const getTvShows = async (e) => {
-  const userSearch = form.elements.query.value.trim();
-  console.log(userSearch);
-  setTimeout(() => {
-    searchFeedback.innerHTML = `You searched for "<span>${userSearch}</span>"`;
-  }, 200);
+  li.innerHTML = `
+            <a href="/details.html?id=${movie.id}">
+               <picture>
+               ${
+                 movie.poster_path
+                   ? `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+              />`
+                   : `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+               }
+               </picture>
+           </a>
+          <div class="card-info flex-col gap-5">
+            <h4 class="page-title body-copy fw-500">${name}</h4>
+            <div class="content-details flex-row gap-10">
+                <p class="quiet-voice">${year}</p>
+                <p class="quiet-voice"><i class="fas fa-star text-primary"></i> ${movie.vote_average.toFixed(
+                  1
+                )}</p>
 
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${userSearch}`
+            </div>
+          </div>
+    `;
+
+  return li;
+}
+
+//fetch data from tvdb api
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'ea38f315243a154fd347ea9eeb849656';
+  const API_URL = 'https://api.themoviedb.org/3';
+
+  const res = await fetch(
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US&page=1&region=US`
   );
 
-  return res.data;
-};
+  const data = await res.json();
+
+  return data;
+}
+async function fetchPopData(endpoint) {
+  const API_KEY = 'ea38f315243a154fd347ea9eeb849656';
+  const API_URL = 'https://api.themoviedb.org/3';
+
+  const res = await fetch(
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&sort_by=latest_release.desc&first_air_date.gte=2023-01-01&with_watch_providers=8&watch_region=US`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+function updateUI() {
+  displayTrending('movie');
+  // displayPopularMovies();
+  // displayUpcomingMovies();
+  console.log('clicked');
+}
+//
+function init() {
+  //   Event Listeners
+  if (globalState === '/index.html' || globalState === '/') {
+    dropDown.addEventListener('click', dropDownSearch);
+    closeSearch.addEventListener('click', closeDropDownSearch);
+    updateUI();
+  }
+
+  tvBtn.addEventListener('click', displayTrendingTV);
+  movieBtn.addEventListener('click', updateUI);
+}
+
+console.log(globalState);
+
+document.addEventListener('DOMContentLoaded', init);
