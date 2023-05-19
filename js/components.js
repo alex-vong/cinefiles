79286c4c2c4f932d429c2d46241bc79523e6bd60
Alex Vong <avong003@gmail.com>
2023-05-18 21:59:10 -0700

    added a masthead container around navbar

--- js/components.js
+++ js/components.js
@@ -4,60 +4,61 @@ const footer = document.querySelectorAll('footer');
 header.forEach((header) => {
   header.innerHTML = `
       <inner-column>
-      <masthead class="flex-col">
-          <div class="nav-bar">
-              <div>
-                  <a href="/index.html">
-                      <section class="about-logo flex-row">
-                          <span class="fourth-level-heading main">C</span>
-                          <span class="fourth-level-heading">I</span>
-                          <span class="fourth-level-heading">N</span>
-                          <span class="fourth-level-heading">E</span>
-                          <span class="fourth-level-heading main">F</span>
-                          <span class="fourth-level-heading">I</span>
-                          <span class="fourth-level-heading">L</span>
-                          <span class="fourth-level-heading">E</span>
-                          <span class="fourth-level-heading">S</span>
-                      </section>
-                  </a>
-                  <nav-links class="quiet-voice">
-                      <a href="/index.html" class="fw-500 home">Home</a>
-                      <a href="" class="about-btn fw-500 about">About</a>
-                      <a href="" class="fw-500 imdb">Top IMDB</a>
-                  </nav-links>
+        <masthead class="flex-col">
+            <div class="nav-bar">
+                <div>
+                    <a href="/index.html">
+                        <section class="about-logo flex-row">
+                            <span class="fourth-level-heading main">C</span>
+                            <span class="fourth-level-heading">I</span>
+                            <span class="fourth-level-heading">N</span>
+                            <span class="fourth-level-heading">E</span>
+                            <span class="fourth-level-heading main">F</span>
+                            <span class="fourth-level-heading">I</span>
+                            <span class="fourth-level-heading">L</span>
+                            <span class="fourth-level-heading">E</span>
+                            <span class="fourth-level-heading">S</span>
+                        </section>
+                    </a>
+                    <nav-links class="quiet-voice">
+                        <a href="/index.html" class="fw-500 home">Home</a>
+                        <a href="" class="about-btn fw-500 about">About</a>
+                        <a href="" class="fw-500 imdb">Top IMDB</a>
+                    </nav-links>
+                </div>
+                <div class="icons">
+                    <i class="fa-solid fa-magnifying-glass search-dd"></i>
+                    <i class="fa-solid fa-bars burger-menu"></i>
+                    <div class="login">
+                        <a href="" class="body-copy login-icon"><i class="fa-regular fa-circle-user"></i></a>
+                    </div>
+                </div>
               </div>
-              <div class="icons">
-                  <i class="fa-solid fa-magnifying-glass search-dd"></i>
-                  <i class="fa-solid fa-bars burger-menu"></i>
-                  <div class="login">
-                      <a href="" class="body-copy login-icon"><i class="fa-regular fa-circle-user"></i></a>
-                  </div>
-              </div>
-          </div>
-          </div>
-      </masthead>
-      <nav-bar class="nav-search slide-out">
-          <nav-inputs>
-              <section class="search">
-                  <div class="container">
-                    <form id="search-form" action="/search.html" class="search-form flex-col gap-10">
-                      <div class="search-flex flex-row gap-10">
-                          <input
-                            type="text"
-                            name="search-term"
-                            id="search-input"
-                            placeholder="Search database"
-                            class="fw-500"
-                          />
-                          <i class="fa-solid fa-magnifying-glass search-bar-icon"></i>
-                          <i class="fa-regular fa-circle-xmark close-search"></i>
-                          <i class="fa-regular fa-circle-user login"></i>
-                        </div>
-                    </form>
-                  </div>
-                </section>
-          </nav-inputs>
-      </nav-bar>
+            </div>
+
+        <nav-bar class="nav-search slide-out">
+            <nav-inputs>
+                    <div class="container">
+                      <form id="search-form" action="/search.html" class="search-form flex-col gap-10">
+                        <div class="search-flex flex-row gap-10">
+                            <input
+                              type="text"
+                              name="search-term"
+                              id="search-input"
+                              placeholder="Search database"
+                              class="fw-500"
+                            />
+                            <i class="fa-solid fa-magnifying-glass search-bar-icon"></i>
+                            <i class="fa-regular fa-circle-xmark close-search"></i>
+                            <i class="fa-regular fa-circle-user login"></i>
+                          </div>
+                      </form>
+                    </div>
+                  
+            </nav-inputs>
+        </nav-bar>
+        </masthead>
+
     </inner-column>
   
   `;
