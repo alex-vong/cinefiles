a5babdc90666cf44f7514f9c4d254119e29cd066
Alex Vong <avong003@gmail.com>
2023-05-25 12:07:43 -0700

    changed unavailable img

--- js/script.js
+++ js/script.js
@@ -631,7 +631,7 @@ function generateCard(content) {
                 alt="${content === 'movie' ? content.title : content.name}"
               />`
                    : `<img
-              src="../images/cf-no-img.svg"
+              src="../images/unavailable.jpg"
               class="card-img-top"
               alt="${content === 'movie' ? content.title : content.name}"
             />`
